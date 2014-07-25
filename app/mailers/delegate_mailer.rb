4a9702b7bb7d75bd7763085b183f527ce6f31cdb
bitcoinerS <bitcoiner@cryptomoon.com>
2014-07-24 23:09:50 -0700

    wip mailer

--- app/mailers/delegate_mailer.rb
+++ app/mailers/delegate_mailer.rb
@@ -1,3 +1,8 @@
+require 'rest_client'
+API_KEY = 'key-ddc1b16b2665e26d9604cb122d1dae77'
+API_URL = "https://api:key-ddc1b16b2665e26d9604cb122d1dae77@api.mailgun.net/v2/bitdelegates.net"
+
+
 class DelegateMailer < ActionMailer::Base
   default from: "mailer@bitdelegates.net"
 
@@ -6,12 +11,21 @@ class DelegateMailer < ActionMailer::Base
   #
   #   en.delegate_mailer.alert_notification.subject
   #
-  def alert_notification(record)
-    @record = record
+  def alert_notification #(record)
+    #@record = record
     @greeting = "BitDelegates Alert"
 
     mail to: "bitcoiner@yandex.com", subject: "Success! You did it."
   end
+
+def alert
+  RestClient.post "https://api:key-ddc1b16b2665e26d9604cb122d1dae77"\
+  "@api.mailgun.net/v2/bitdelegates.net/messages",
+  :from => "<mailer@bitdelegates.net>",
+  :to => "slavixtube@yandex.com",
+  :subject => "BitDelegates Alert",
+  :html => "<b>HTML</b> version of the body!"
+end
 
 
 end
